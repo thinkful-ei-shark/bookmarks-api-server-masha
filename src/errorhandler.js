ec759b4f9f5b6195f43d29815f68011689cc1e2f
Marc Trius <derpayatz@gmail.com>
2020-10-23 10:38:00 -0500

    configure winston

--- src/errorhandler.js
+++ src/errorhandler.js
@@ -1,12 +1,14 @@
-module.exports = () =>
+const logger = require('./logger');
+
 // eslint-disable-next-line no-unused-vars
-  (error, req, res, next) => {
-    let response;
-    if (process.env.NODE_ENV === 'production') {
-      response = { error: { message: 'server error' } };
-    } else {
-      console.error(error);
-      response = { message: error.message, error };
-    }
-    res.status(500).json(response);
-  };
+module.exports = function errorHandler(error, req, res, next) {
+  logger.error(error);
+
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+};
