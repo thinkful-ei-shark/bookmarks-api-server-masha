38d3a71ec3448f1097809bb604000ebaa5a57d01
Marc Trius <derpayatz@gmail.com>
2020-10-23 13:18:39 -0500

    implement POST /

--- src/bookmark-route.js
+++ src/bookmark-route.js
@@ -1,5 +1,8 @@
 const bookmarkRouter = require('express').Router();
+const parseJson = require('express').json();
+const uuid = require('uuid').v4;
 
+const testUrl = require('./test-url');
 const data = require('./data-store');
 
 // GET /
@@ -9,10 +12,45 @@ bookmarkRouter
     res
       .status(200)
       .json(data);
+  })
+
+  // POST /
+  .post(parseJson, (req, res) => {
+    const contentType = req.headers['content-type'];
+    const { title, url, desc, rating } = req.body;
+    if (!contentType || !contentType.includes('json')) {
+      return res
+        .status(400)
+        .json({ message: 'Request must include JSON body' });
+    }
+    if (!title) {
+      return res
+        .status(400)
+        .json({ message: 'Title required' });
+    }
+    if (!url || !testUrl(url)) {
+      return res
+        .status(400)
+        .json({ message: 'Valid URL required' });
+    }
+    if (rating && !parseInt(rating)) {
+      return res
+        .status(400)
+        .json({ message: 'Rating must be a number' });
+    }
+    
+    const newBookmark = {
+      id: uuid(),
+      title,
+      url,
+      desc: desc || '',
+      rating: parseInt(rating) || null
+    };
+    data.push(newBookmark);
+    return res
+      .status(200)
+      .json(newBookmark);
   });
-
-
-// POST /
 // PATCH /:id
 // DELETE /:id
 
