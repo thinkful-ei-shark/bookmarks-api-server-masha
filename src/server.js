310169728272641887d6b613a2789c4e434628e4
Marc Trius <derpayatz@gmail.com>
2020-10-29 12:33:26 -0500

    add database and migrations support

--- src/server.js
+++ src/server.js
@@ -1,6 +1,17 @@
 require('dotenv').config();
+const knex = require('knex');
 
-const { NODE_ENV, PORT } = require('./config');
+const { NODE_ENV, PORT, DB_URL } = require('./config');
 const app = require('./app');
+
+app.set(
+  'db',
+  knex(
+    {
+      client: 'pg',
+      connection: DB_URL
+    }
+  )
+);
 
 app.listen(PORT, () => console.log(`Server started in ${NODE_ENV} mode on ${PORT}...`));
