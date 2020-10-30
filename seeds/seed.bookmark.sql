bfe8d40dc92903d0c6fe4df69cd8e7a9b6334df2
Marc Trius <derpayatz@gmail.com>
2020-10-30 11:26:34 -0500

    add seeding script

---
+++ seeds/seed.bookmark.sql
@@ -0,0 +1,10 @@
+INSERT INTO public.bookmark
+(title, url, description, rating)
+VALUES(Google, https://google.com, Search Monster, 4),
+(Facebook, https://facebook.com, Social Media Oligopoly, 3),
+(Twitter, https://twitter.com, Social Media Oligopoly #2, 3),
+(Jews International, https://jews.international, Best Blog, 5),
+(Jewish Currents, https://jewishcurrents.org, An interesting mag, 4),
+(Thinkful, https://thinkful.com, Pretty decent people, 4),
+(Liderbukh, https://liderbukh.jews.international, My biggest app, 5),
+(It''s Going Down, https://itsgoingdown.org, NULL, NULL);
