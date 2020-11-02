08c29f4ed8a15f8f316d8d6eae076d83947e94b2
Marc Trius <derpayatz@gmail.com>
2020-11-02 12:43:15 -0600

    fix seed script

--- seeds/seed.bookmark.sql
+++ seeds/seed.bookmark.sql
@@ -1,10 +1,10 @@
-INSERT INTO public.bookmark
-(title, url, description, rating)
-VALUES(Google, https://google.com, Search Monster, 4),
-(Facebook, https://facebook.com, Social Media Oligopoly, 3),
-(Twitter, https://twitter.com, Social Media Oligopoly #2, 3),
-(Jews International, https://jews.international, Best Blog, 5),
-(Jewish Currents, https://jewishcurrents.org, An interesting mag, 4),
-(Thinkful, https://thinkful.com, Pretty decent people, 4),
-(Liderbukh, https://liderbukh.jews.international, My biggest app, 5),
-(It''s Going Down, https://itsgoingdown.org, NULL, NULL);
+INSERT INTO bookmark
+(bm_title, bm_url, bm_description, bm_rating)
+VALUES('Google', 'https://google.com', 'Search Monster', 4),
+('Facebook', 'https://facebook.com', 'Social Media Oligopoly', 3),
+('Twitter', 'https://twitter.com', 'Social Media Oligopoly', 3),
+('Jews International', 'https://jews.international', 'Best Blog', 5),
+('Jewish Currents', 'https://jewishcurrents.org', 'An interesting mag', 4),
+('Thinkful', 'https://thinkful.com', 'Pretty decent people', 4),
+('Liderbukh', 'https://liderbukh.jews.international', 'My biggest app', 5),
+('It''s Going Down', 'https://itsgoingdown.org', NULL, 5);
